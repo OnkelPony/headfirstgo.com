2b67e764a2e02ce9294f2302a6585d7d522fb1b6
Jiří Martínek <zwo@centrum.cz>
2020-03-29 22:09:05 +0200

    Chapter 9 complete.

---
+++ source/head-first-go/ch09/exercise1.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type rectangle struct {
+	length float64
+	width  float64
+}
+
+// YOUR CODE HERE: Convert this function to a method on
+// the "rectangle" type named "info".
+func (r *rectangle) info() {
+	fmt.Println("Length:", (*r).length)
+	fmt.Println("Width:", r.width)
+}
+
+func (r *rectangle) makeSquare() {
+	if r.length < r.width {
+		r.width = r.length
+	} else {
+		r.length = r.width
+	}
+}
+func main() {
+	var r rectangle
+	r.length = 4.2
+	r.width = 2.3
+	// YOUR CODE HERE: Update this function call to a
+	// method call.
+	r.info()
+	r.makeSquare()
+	r.info()
+}
